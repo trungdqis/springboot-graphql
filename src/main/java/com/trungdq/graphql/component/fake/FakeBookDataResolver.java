7cd0f9451c8128813918a1797537b09ab5e7088c
trungdan <trungdang@silverlakeaxis.com>
2023-03-15 00:19:37 +0700

    data resolver from gradle plugin

--- src/main/java/com/trungdq/graphql/component/fake/FakeBookDataResolver.java
+++ src/main/java/com/trungdq/graphql/component/fake/FakeBookDataResolver.java
@@ -4,10 +4,15 @@ import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.InputArgument;
 import com.trungdq.graphql.datasource.fake.FakeBookDataSource;
+import com.trungdq.graphql.generated.DgsConstants;
 import com.trungdq.graphql.generated.types.Book;
+import com.trungdq.graphql.generated.types.ReleaseHistory;
+import com.trungdq.graphql.generated.types.ReleaseHistoryInput;
+import graphql.schema.DataFetchingEnvironment;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -24,5 +29,26 @@ public class FakeBookDataResolver {
                 .filter(b -> StringUtils.containsIgnoreCase(
                         b.getAuthor().getName(), authorName.get()
                 )).collect(Collectors.toList());
+    }
+
+    @DgsData(
+            parentType = DgsConstants.QUERY_TYPE,
+            field = DgsConstants.QUERY.BooksByReleased
+    )
+    public List<Book> getBooksByReleased(DataFetchingEnvironment dataFetchingEnvironment) {
+        var releasedMap = (Map<String, Object>) dataFetchingEnvironment.getArgument("releasedInput");
+        var releasedInput = ReleaseHistoryInput.newBuilder()
+                .printedEdition((boolean) releasedMap.get(DgsConstants.RELEASEHISTORYINPUT.PrintedEdition))
+                .year((int) releasedMap.get(DgsConstants.RELEASEHISTORYINPUT.Year))
+                .build();
+
+        return FakeBookDataSource.BOOK_LIST.stream()
+                .filter(
+                        b -> this.matchReleaseHistory(releasedInput, b.getReleased())
+                ).collect(Collectors.toList());
+    }
+
+    private boolean matchReleaseHistory(ReleaseHistoryInput input, ReleaseHistory element) {
+        return input.getPrintedEdition().equals(element.getPrintedEdition()) && input.getYear() == element.getYear();
     }
 }
